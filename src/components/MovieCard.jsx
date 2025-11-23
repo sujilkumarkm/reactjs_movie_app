e6037dea416d7a3b1a45bc1ec5d864b4f9834db9
sujilkumarkm <kmsujilkumar@gmail.com>
2025-11-23 09:06:14 +0000

    movie card image and date added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,14 +8,14 @@ function MovieCard({movie})
     }
     return <div className="movie-card">
         <div className="movie-poster">
-            <img src="{movie.url}" alt="{movie.title}"></img>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="{movie.title}"/>
             <div className="movie-overlay">
                 <button className="favorite-icon" onClick={onFavoriteClick}>Click to Like</button>
             </div>
         </div>
         <div className="movie-info">
             <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
+            <p>{movie.release_date?.split("-")[0]}</p>
         </div>
     </div>
 }
